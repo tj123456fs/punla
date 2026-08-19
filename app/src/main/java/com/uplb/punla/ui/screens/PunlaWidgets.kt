f12a6282a71d0ecbdabfd73e019df5febda6c240
Tj <montefalcontj@gmail.com>
2026-08-19 12:26:13 +0800

    Fix Compose color animation build error

--- app/src/main/java/com/uplb/punla/ui/screens/PunlaWidgets.kt
+++ app/src/main/java/com/uplb/punla/ui/screens/PunlaWidgets.kt
@@ -1,6 +1,6 @@
 package com.uplb.punla.ui.screens
 
-import androidx.compose.animation.core.animateColorAsState
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
